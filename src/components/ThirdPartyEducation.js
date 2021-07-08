f391c474e285aa6accb7bc385ae5cd32c6c8d8a9
snshamwow <73720206+snshamwow@users.noreply.github.com>
2021-07-08 19:38:55 -0400

    Add files via upload
    Implementing a search feature using NewsAPI to find crypto news.

--- src/components/ThirdPartyEducation.js
+++ src/components/ThirdPartyEducation.js
@@ -1,13 +1,73 @@
 import React from "react";
-import { Card } from "react-bootstrap"
+import { Card } from "react-bootstrap" // npm i bootstrap react-bootstrap
 
 export const ThirdPartyEducation = () => {
 
-    return (
+
+
+    function retrieve(e){
+        e.preventDefault()
+
+        const searchFrom = document.querySelector('search');
+        const input = document.querySelector('.input');
+        const newsList = document.querySelector('.news-list');
+
+        if (input.value === ''){
+            alert('Input field is empty.')
+            return
+        }
+      
+        newsList.innerHTML = ''
+      
+        const apiKey = '8577c03e48d246b3b0ca7329456e3e84'
+        
+        let topic = input.value
+        let url = `https://newsapi.org/v2/everything?q=${topic}&apiKey=${apiKey}`
+      
+        fetch(url).then((res)=>{
+          return res.json()
+          }).then((data)=>{
+              console.log(data)
+      
+              data.articles.forEach(article =>{
+      
+                let li = document.createElement('li');
+                let a = document.createElement('a');
+      
+                a.setAttribute('href', article.url);
+                a.setAttribute('target', '_blank');
+      
+                a.textContent = article.title;
+      
+                li.appendChild(a);
+      
+                newsList.appendChild(li);
+              })
+          })
+          .catch((error)=>{
+      
+          })
+      
+          console.log(topic)
+          input.innerHTML = ''
+      }
+
+      return (
         <Card>
             <Card.Header> Third Party Education</Card.Header>
             <Card.Body>
+
+                <form onSubmit={(e)=>retrieve(e)} className="search" action ="">
+                    <input placeholder= "Search Cryptos"className ="input" type="text" /><br/>
+                    <input type="submit"/> 
+                </form>
+
+                <div className="container">
+                    <ul className="news-list"></ul>
+                </div>
+
             </Card.Body>
         </Card>
-    )
+      )
+    
 }
